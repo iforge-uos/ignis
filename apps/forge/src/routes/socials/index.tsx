f4f72b3f777099fa0f0eaf2c90fa59bccb912c53
Sam Piper <sampii@protonmail.com>
2024-04-16 01:28:14 +0100

    fix: more unfinished routes

--- apps/forge/src/routes/socials/index.tsx
+++ apps/forge/src/routes/socials/index.tsx
@@ -1,18 +1,6 @@
-import {createFileRoute} from "@tanstack/react-router";
-import Title from "@/components/title";
+import { createFileRoute } from "@tanstack/react-router";
+import { RouteUnfinished } from "@/components/routing/RouteUnfinished.tsx";
 
-const SocialsIndexPageComponent = () => {
-    return (
-        <>
-            <Title prompt="SOCIALS"/>
-            <div className="p-2">
-                <h3>iForge Social Media Page</h3>
-                <p>WIP</p>
-            </div>
-        </>
-    );
-};
-
-export const Route = createFileRoute('/socials/')({
-    component: SocialsIndexPageComponent,
+export const Route = createFileRoute("/socials/")({
+  component: RouteUnfinished,
 });
--- apps/forge/src/routes/training/approved-materials.tsx
+++ apps/forge/src/routes/training/approved-materials.tsx
@@ -1,5 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { RouteUnfinished } from "@/components/routing/RouteUnfinished.tsx";
 
-export const Route = createFileRoute('/training/approved-materials')({
-  component: () => <div>Hello /training/approved-materials!</div>
-})
+export const Route = createFileRoute("/training/approved-materials")({
+  component: RouteUnfinished,
+});
