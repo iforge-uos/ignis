16b45a01091596306c2ad7e3b04bf085547a55b9
Sam Piper <sampii@protonmail.com>
2024-04-17 19:12:40 +0100

    feat: new home page

--- apps/forge/src/routes/_authenticated/signin/home/index.tsx
+++ apps/forge/src/routes/_authenticated/signin/home/index.tsx
@@ -2,19 +2,404 @@ import ActiveLocationSelector from "@/components/signin/ActiveLocationSelector";
 import Title from "@/components/title";
 import { useUser } from "@/lib/utils.ts";
 import { createFileRoute } from "@tanstack/react-router";
-import { BentoGrid, BentoGridItem } from "@ui/components/bento-grid.tsx";
-import { Bitcoin, MessageCircleMore } from "lucide-react";
+import { Book, Coins, MessageCircleMore } from "lucide-react";
+import { World } from "@ui/components/globe.tsx";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@ui/components/ui/card.tsx";
+import { Button } from "@ui/components/ui/button.tsx";
+
+const globeConfig = {
+  pointSize: 4,
+  globeColor: "#062056",
+  showAtmosphere: true,
+  atmosphereColor: "#FFFFFF",
+  atmosphereAltitude: 0.1,
+  emissive: "#062056",
+  emissiveIntensity: 0.1,
+  shininess: 0.9,
+  polygonColor: "rgba(255,255,255,0.7)",
+  ambientLight: "#38bdf8",
+  directionalLeftLight: "#ffffff",
+  directionalTopLight: "#ffffff",
+  pointLight: "#ffffff",
+  arcTime: 1000,
+  arcLength: 0.9,
+  rings: 1,
+  maxRings: 3,
+  initialPosition: { lat: 22.3193, lng: 114.1694 },
+  autoRotate: true,
+  autoRotateSpeed: 0.5,
+};
+const colors = ["#06b6d4", "#3b82f6", "#6366f1"];
+const sampleArcs = [
+  {
+    order: 1,
+    startLat: -19.885592,
+    startLng: -43.951191,
+    endLat: -22.9068,
+    endLng: -43.1729,
+    arcAlt: 0.1,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 1,
+    startLat: 28.6139,
+    startLng: 77.209,
+    endLat: 3.139,
+    endLng: 101.6869,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 1,
+    startLat: -19.885592,
+    startLng: -43.951191,
+    endLat: -1.303396,
+    endLng: 36.852443,
+    arcAlt: 0.5,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 2,
+    startLat: 1.3521,
+    startLng: 103.8198,
+    endLat: 35.6762,
+    endLng: 139.6503,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 2,
+    startLat: 51.5072,
+    startLng: -0.1276,
+    endLat: 3.139,
+    endLng: 101.6869,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 2,
+    startLat: -15.785493,
+    startLng: -47.909029,
+    endLat: 36.162809,
+    endLng: -115.119411,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 3,
+    startLat: -33.8688,
+    startLng: 151.2093,
+    endLat: 22.3193,
+    endLng: 114.1694,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 3,
+    startLat: 21.3099,
+    startLng: -157.8581,
+    endLat: 40.7128,
+    endLng: -74.006,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 3,
+    startLat: -6.2088,
+    startLng: 106.8456,
+    endLat: 51.5072,
+    endLng: -0.1276,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 4,
+    startLat: 11.986597,
+    startLng: 8.571831,
+    endLat: -15.595412,
+    endLng: -56.05918,
+    arcAlt: 0.5,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 4,
+    startLat: -34.6037,
+    startLng: -58.3816,
+    endLat: 22.3193,
+    endLng: 114.1694,
+    arcAlt: 0.7,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 4,
+    startLat: 51.5072,
+    startLng: -0.1276,
+    endLat: 48.8566,
+    endLng: -2.3522,
+    arcAlt: 0.1,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 5,
+    startLat: 14.5995,
+    startLng: 120.9842,
+    endLat: 51.5072,
+    endLng: -0.1276,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 5,
+    startLat: 1.3521,
+    startLng: 103.8198,
+    endLat: -33.8688,
+    endLng: 151.2093,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 5,
+    startLat: 34.0522,
+    startLng: -118.2437,
+    endLat: 48.8566,
+    endLng: -2.3522,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 6,
+    startLat: -15.432563,
+    startLng: 28.315853,
+    endLat: 1.094136,
+    endLng: -63.34546,
+    arcAlt: 0.7,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 6,
+    startLat: 37.5665,
+    startLng: 126.978,
+    endLat: 35.6762,
+    endLng: 139.6503,
+    arcAlt: 0.1,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 6,
+    startLat: 22.3193,
+    startLng: 114.1694,
+    endLat: 51.5072,
+    endLng: -0.1276,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 7,
+    startLat: -19.885592,
+    startLng: -43.951191,
+    endLat: -15.595412,
+    endLng: -56.05918,
+    arcAlt: 0.1,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 7,
+    startLat: 48.8566,
+    startLng: -2.3522,
+    endLat: 52.52,
+    endLng: 13.405,
+    arcAlt: 0.1,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 7,
+    startLat: 52.52,
+    startLng: 13.405,
+    endLat: 34.0522,
+    endLng: -118.2437,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 8,
+    startLat: -8.833221,
+    startLng: 13.264837,
+    endLat: -33.936138,
+    endLng: 18.436529,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 8,
+    startLat: 49.2827,
+    startLng: -123.1207,
+    endLat: 52.3676,
+    endLng: 4.9041,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 8,
+    startLat: 1.3521,
+    startLng: 103.8198,
+    endLat: 40.7128,
+    endLng: -74.006,
+    arcAlt: 0.5,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 9,
+    startLat: 51.5072,
+    startLng: -0.1276,
+    endLat: 34.0522,
+    endLng: -118.2437,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 9,
+    startLat: 22.3193,
+    startLng: 114.1694,
+    endLat: -22.9068,
+    endLng: -43.1729,
+    arcAlt: 0.7,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 9,
+    startLat: 1.3521,
+    startLng: 103.8198,
+    endLat: -34.6037,
+    endLng: -58.3816,
+    arcAlt: 0.5,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 10,
+    startLat: -22.9068,
+    startLng: -43.1729,
+    endLat: 28.6139,
+    endLng: 77.209,
+    arcAlt: 0.7,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 10,
+    startLat: 34.0522,
+    startLng: -118.2437,
+    endLat: 31.2304,
+    endLng: 121.4737,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 10,
+    startLat: -6.2088,
+    startLng: 106.8456,
+    endLat: 52.3676,
+    endLng: 4.9041,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 11,
+    startLat: 41.9028,
+    startLng: 12.4964,
+    endLat: 34.0522,
+    endLng: -118.2437,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 11,
+    startLat: -6.2088,
+    startLng: 106.8456,
+    endLat: 31.2304,
+    endLng: 121.4737,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 11,
+    startLat: 22.3193,
+    startLng: 114.1694,
+    endLat: 1.3521,
+    endLng: 103.8198,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 12,
+    startLat: 34.0522,
+    startLng: -118.2437,
+    endLat: 37.7749,
+    endLng: -122.4194,
+    arcAlt: 0.1,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 12,
+    startLat: 35.6762,
+    startLng: 139.6503,
+    endLat: 22.3193,
+    endLng: 114.1694,
+    arcAlt: 0.2,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 12,
+    startLat: 22.3193,
+    startLng: 114.1694,
+    endLat: 34.0522,
+    endLng: -118.2437,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 13,
+    startLat: 52.52,
+    startLng: 13.405,
+    endLat: 22.3193,
+    endLng: 114.1694,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 13,
+    startLat: 11.986597,
+    startLng: 8.571831,
+    endLat: 35.6762,
+    endLng: 139.6503,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 13,
+    startLat: -22.9068,
+    startLng: -43.1729,
+    endLat: -34.6037,
+    endLng: -58.3816,
+    arcAlt: 0.1,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+  {
+    order: 14,
+    startLat: -33.936138,
+    startLng: 18.436529,
+    endLat: 21.395643,
+    endLng: 39.883798,
+    arcAlt: 0.3,
+    color: colors[Math.floor(Math.random() * (colors.length - 1))],
+  },
+];
 
 const SignInAppComponent = () => {
-  const Skeleton = () => (
-    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  );
   const items = [
     {
       title: "iForge Discord",
       description: "Join the iForge Discord if you haven't already.",
-      header: <Skeleton />,
-      icon: <MessageCircleMore className="h-4 w-4 text-neutral-500" />,
+      icon: <MessageCircleMore className="h-8 w-8 text-neutral-500" />,
+      linkText: "Join",
       externalLink: import.meta.env.VITE_DISCORD_URL,
     },
   ];
@@ -25,16 +410,16 @@ const SignInAppComponent = () => {
       {
         title: "Purchase Form",
         description: "Discover the beauty of thoughtful and functional design.",
-        header: <Skeleton />,
-        icon: <Bitcoin className="h-4 w-4 text-neutral-500" />,
+        icon: <Coins className="h-8 w-8 text-neutral-500" />,
+        linkText: "Buy",
         externalLink:
           "https://docs.google.com/forms/d/e/1FAIpQLScdLTE7eXqGQRa3e0UfymYo8qjlNTyu5xfIyArMG0wGQgHjyw/viewform",
       },
       {
         title: "iDocs",
-        description: "Understand the impact of effective communication in our lives.",
-        header: <Skeleton />,
-        icon: <Bitcoin className="h-4 w-4 text-neutral-500" />,
+        description: "Check out your team specific documentation here (coming soon)",
+        icon: <Book className="h-8 w-8 text-neutral-500" />,
+        linkText: "Learn",
         externalLink: "https://docs.iforge.shef.ac.uk",
       },
     );
@@ -46,22 +431,34 @@ const SignInAppComponent = () => {
       <div className="p-4 mt-1">
         <ActiveLocationSelector />
         <div className="border-2 p-4">
-          <h1 className="text-xl font-bold mb-4 text-center">
-            Sign In Home (Please excuse the visual inconsistencies)
-          </h1>
-          <BentoGrid className="max-w-4xl mx-auto">
-            {items.map((item, i) => (
-              <BentoGridItem
-                key={i}
-                title={item.title}
-                description={item.description}
-                header={item.header}
-                icon={item.icon}
-                className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-                externalLink={item.externalLink}
-              />
-            ))}
-          </BentoGrid>
+          <h1 className="text-xl font-bold mb-4 text-center">Sign in Home</h1>
+          <div className="flex flex-wrap-reverse">
+            <div className="w-1/2">
+              <div className="aspect-[1/1]">
+                <World data={sampleArcs} globeConfig={globeConfig} />
+              </div>
+            </div>
+            <div className="w-1/2 justify-items-center">
+              <div className="space-x-1 space-y-1 grid grid-cols-1 gap-4">
+                {items.map((item) => (
+                  <Card className="max-w-xl">
+                    <CardHeader>
+                      <CardTitle className="flex flex-wrap">
+                        {item.icon}
+                        <span className={"ml-2"}>{item.title}</span>
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>{item.description}</CardContent>
+                    <CardFooter>
+                      <a href={item.externalLink} target="_blank" rel="noopener noreferrer">
+                        <Button>{item.linkText}</Button>
+                      </a>
+                    </CardFooter>
+                  </Card>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
