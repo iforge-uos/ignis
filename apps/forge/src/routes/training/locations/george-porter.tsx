4725097137792b857550cc0b14c386d5b5cc9936
Gobot1234 <gobot1234yt@gmail.com>
2024-05-08 17:49:13 +0100

    fix: you saw nothing

--- apps/forge/src/routes/training/locations/george-porter.tsx
+++ apps/forge/src/routes/training/locations/george-porter.tsx
@@ -1,11 +1,13 @@
 import TrainingLocation from "@/components/training/TrainingLocation";
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 const GeorgePorter = () => (
-  <Link to="https://training.iforge.shef.ac.uk/subject-areas/george-porter-cca-rep/online" />
-  // <TrainingLocation location={"GEORGE_PORTER"} optionalTrainingText={"carbon fibre layup station"} />
+  <TrainingLocation location={"GEORGE_PORTER"} optionalTrainingText={"carbon fibre layup station"} />
 );
 
 export const Route = createFileRoute("/training/locations/george-porter")({
+  beforeLoad: () => {
+    return redirect({to: "https://training.iforge.shef.ac.uk/subject-areas/george-porter-cca-rep/online"})
+  },
   component: GeorgePorter,
 });
