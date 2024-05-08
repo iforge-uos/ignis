5694d026cf346d40a0cadd3bd93d1f375d685e71
Gobot1234 <gobot1234yt@gmail.com>
2024-05-08 17:46:04 +0100

    feat: make george porter training point to old site for now

--- apps/forge/src/routes/training/locations/george-porter.tsx
+++ apps/forge/src/routes/training/locations/george-porter.tsx
@@ -1,8 +1,9 @@
 import TrainingLocation from "@/components/training/TrainingLocation";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 const GeorgePorter = () => (
-  <TrainingLocation location={"GEORGE_PORTER"} optionalTrainingText={"carbon fibre layup station"} />
+  <Link to="https://training.iforge.shef.ac.uk/subject-areas/george-porter-cca-rep/online" />
+  // <TrainingLocation location={"GEORGE_PORTER"} optionalTrainingText={"carbon fibre layup station"} />
 );
 
 export const Route = createFileRoute("/training/locations/george-porter")({
