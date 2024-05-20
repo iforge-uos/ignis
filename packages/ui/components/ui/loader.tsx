7c3043806541b9e6199f397422c3e316039fb1a3
Gobot1234 <gobot1234yt@gmail.com>
2024-05-20 17:18:29 +0100

    feat: more customisation for the loader

--- packages/ui/components/ui/loader.tsx
+++ packages/ui/components/ui/loader.tsx
@@ -1,11 +1,10 @@
-import {PulseLoader} from "react-spinners";
+import { cn } from "@/lib/utils";
+import { PulseLoader } from "react-spinners";
 
-export const Loader = () => {
-    return (
-        <section className="h-full w-full flex justify-center items-center">
-            <PulseLoader color="#e11d48" size={20}
-            />
-        </section>
-    );
+export const Loader = ({ className, size }: { className?: string; size?: number }) => {
+  return (
+    <section className={cn("h-full w-full flex justify-center items-center", className)}>
+      <PulseLoader color="#e11d48" size={size ?? 20} />
+    </section>
+  );
 };
-
