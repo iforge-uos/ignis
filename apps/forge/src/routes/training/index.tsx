730c7d17bae7a553fd30fef1da74a2a3b6e860d8
Sam Piper <sampii@protonmail.com>
2024-05-23 20:04:05 +0100

    feat: IndexCard component setup for the training index

--- apps/forge/src/routes/training/index.tsx
+++ apps/forge/src/routes/training/index.tsx
@@ -1,15 +1,13 @@
 import Title from "@/components/title";
-import { useUser } from "@/lib/utils";
-import { Link, createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@ui/components/ui/button";
-import { Card, CardContent, CardDescription, CardTitle } from "@ui/components/ui/card";
 import { Separator } from "@ui/components/ui/separator";
-import React from "react";
+import IndexCard from "@/routes/training/-components/IndexCard.tsx";
 
 export default function Component() {
   return (
-    <>
-      <Title prompt="Training" />
+      <>
+      <Title prompt="Training"/>
       <div className="p-4 mt-1">
         <div className="text-center mb-8">
           <h1 className="text-4xl font-bold tracking-wide sm:text-5xl md:text-6xl">
@@ -21,110 +19,64 @@ export default function Component() {
           <h2 className="text-2xl font-semibold text-center mb-4">Search for a course</h2>
           <div className="relative w-3/4 mx-auto">
             <input
-              placeholder="Enter all or part of a course name..."
-              className="border-2 bg-inherit border-gray-300 rounded-md py-2 px-4 w-full ring-offset-background placeholder:text-muted-foreground outline-primary"
+                placeholder="Enter all or part of a course name..."
+                className="border-2 bg-inherit border-gray-300 rounded-md py-2 px-4 w-full ring-offset-background placeholder:text-muted-foreground outline-primary"
             />
             <Button className="absolute top-0 right-0 m-0.5">Search</Button>
           </div>
         </div>
         <h4 className="text-3xl font-semibold mb-4">Locations</h4>
         <div className="grid grid-cols-3 gap-4 align-middle">
-          <Card className="w-full transition-all duration-300 hover:bg-accent group flex flex-col gap-2.5 p-4 border rounded-lg md:gap-4 md:p-8">
-            <br />
-            <Link to="/training/locations/mainspace">
-              <CardContent className="m-4">
-                <CardTitle className="text-3xl">iForge Mainspace</CardTitle>
-                <Separator />
-                <br />
-                <img
-                  alt="iForge Mainspace"
-                  height="100"
-                  src={`${import.meta.env.VITE_CDN_URL}/files/mainspace_training.jpg`}
-                  className="aspect-[2/1] object-cover rounded-md transition-all duration-300 group-hover:grayscale"
-                />
-                <CardDescription className="text-lg">
-                  Training for the iForge Mainspace located in the Diamond.
-                </CardDescription>
-              </CardContent>
-            </Link>
-          </Card>
-          <Card className="w-full hover:bg-accent">
-            <Link to="/training/locations/heartspace">
-              <CardContent className="m-4">
-                <img
-                  alt="iForge Heartspace"
-                  height="100"
-                  src="/placeholder.svg"
-                  className="aspect-[2/1] object-cover"
-                />
-                <CardTitle>iForge Heartspace</CardTitle>
-                <CardDescription>Training for the iForge Mainspace located in the Heartspace.</CardDescription>
-              </CardContent>
-            </Link>
-          </Card>
-          <Card className="w-full hover:bg-accent">
-            {/* <Link to="/training/locations/george-porter"> */}
-            <CardContent
-              className="m-4"
+          <IndexCard
+              to="/training/locations/mainspace"
+              imageSrc={`${import.meta.env.VITE_CDN_URL}/files/mainspace_training.jpg`}
+              imageAlt="iForge Mainspace"
+              title="iForge Mainspace"
+              description="Training for the iForge Mainspace located in the Diamond."
+          />
+          <IndexCard
+              to="/training/locations/heartspace"
+              imageSrc="/placeholder.svg"
+              imageAlt="iForge Heartspace"
+              title="iForge Heartspace"
+              description="Training for the iForge Mainspace located in the Heartspace."
+          />
+          <IndexCard
               onClick={() =>
-                (window.location.href = "https://training.iforge.shef.ac.uk/subject-areas/george-porter-cca-rep/online")
+                  (window.location.href = "https://training.iforge.shef.ac.uk/subject-areas/george-porter-cca-rep/online")
               }
-            >
-              <img
-                alt="George Porter CCA Workshop"
-                src="/placeholder.svg"
-                className="aspect-[2/1] object-cover"
-                height="100"
-              />
-              <CardTitle>George Porter CCA Workshop</CardTitle>
-              <CardDescription>
-                Training appropriate for CCA members looking to gain access to the iTec and its training.
-              </CardDescription>
-            </CardContent>
-            {/* </Link> */}
-          </Card>
+              imageSrc="/placeholder.svg"
+              imageAlt="George Porter CCA Workshop"
+              title="George Porter CCA Workshop"
+              description="Training appropriate for CCA members looking to gain access to the iTec and its training."
+          />
         </div>
-        <br />
-        <Separator />
+        <br/>
+        <Separator/>
         <h3 className="text-2xl font-semibold text-center m-4">Useful links</h3>
-        <br />
+        <br/>
         <div>
           <div className="grid grid-cols-2 gap-4 align-middle">
-            <Card className="w-full hover:bg-accent">
-              <Link to="/training/approved-materials">
-                <CardContent className="m-4">
-                  <img
-                    alt="Approved materials"
-                    height="100"
-                    src="/placeholder.svg"
-                    className="aspect-square object-cover"
-                    width="100"
-                  />
-                  <CardTitle>Approved Materials</CardTitle>
-                  <CardDescription>COSHH regulations for materials in the iForge.</CardDescription>
-                </CardContent>
-              </Link>
-            </Card>
-            <Card className="w-full hover:bg-accent">
-              <Link to="/training/risk-assessments">
-                <CardContent className="m-4">
-                  <img
-                    alt="iForge Risk Assessment"
-                    height="100"
-                    src="/placeholder.svg"
-                    className="aspect-square object-cover"
-                    width="100"
-                  />
-                  <CardTitle>iForge Risk Assessment</CardTitle>
-                  <CardDescription>The various risk assessments for the spaces.</CardDescription>
-                </CardContent>
-              </Link>
-            </Card>
+            <IndexCard
+                to="/training/approved-materials"
+                imageSrc="/placeholder.svg"
+                imageAlt="Approved materials"
+                title="Approved Materials"
+                description="COSHH regulations for materials in the iForge."
+            />
+            <IndexCard
+                to="/training/risk-assessments"
+                imageSrc="/placeholder.svg"
+                imageAlt="iForge Risk Assessment"
+                title="iForge Risk Assessment"
+                description="The various risk assessments for the spaces."
+            />
           </div>
         </div>
       </div>
-    </>
-  );
+</>
+)
+  ;
 }
 
 export const Route = createFileRoute("/training/")({
