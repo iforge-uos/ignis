593f050a8386e9887ac146aab98ddc75d3d4b95b
Gobot1234 <gobot1234yt@gmail.com>
2024-05-26 00:47:49 +0100

    feat: initial work on the sign in display

--- packages/types/root.ts
+++ packages/types/root.ts
@@ -1,7 +1,23 @@
+import { sign_in } from "@dbschema/interfaces";
+import { PartialReason } from "./sign_in";
+import { PartialUser } from "./users";
+
 export type Agreement = {
   id: string;
   created_at: Date;
   content: string;
   version: number;
   reasons: { name: string }[];
+};
+
+export type SignIn = {
+  user: PartialUser;
+  reason: PartialReason;
+  id: string;
+  created_at: Date;
+  ends_at: Date | null;
+  duration: number;
+  location: sign_in.SignInLocation;
+  signed_out: boolean | null;
+  tools: string[];
 };
