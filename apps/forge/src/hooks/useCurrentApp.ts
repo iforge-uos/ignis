88d4277ebbab5f42f6c197178b5d5b1e5634fc06
James Hilton-Balfe <gobot1234yt@gmail.com>
2024-05-30 16:48:25 +0100

    chore: get rid of some console calls

--- apps/forge/src/hooks/useCurrentApp.ts
+++ apps/forge/src/hooks/useCurrentApp.ts
@@ -2,27 +2,16 @@ import { Apps } from "@/types/app.ts";
 import { useMatches } from "@tanstack/react-router";
 
 const useCurrentApp = (): Apps | undefined => {
-  console.log("useCurrentApp hook called");
-
-  const matches = useMatches();
-  console.log("useMatches result:", matches);
-
-  const matchWithTitle = matches
+  const matchWithTitle = useMatches()
       .reverse()
-      .find((d) => {
-        console.log("Checking match:", d);
-        return d.staticData?.title;
-      });
+      .find((d) => d.staticData?.title);
 
   if (!matchWithTitle) {
     console.error("No match with title found");
     throw new Error("Page has no title. This should have been set in the root directory in a file with the app's name");
   }
 
-  const title = matchWithTitle.staticData.title;
-  console.log("Found title:", title);
-
-  return title;
+  return matchWithTitle.staticData.title;
 };
 
 export default useCurrentApp;
