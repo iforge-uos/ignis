65fc6f0fb34dd87381b8bec9dd8d42f7d6c4514d
Gobot1234 <gobot1234yt@gmail.com>
2024-05-31 19:01:45 +0100

    feat: improve overall error experience
    show nav bar at all times and cleanup a bit

--- apps/forge/src/components/routing/Forbidden.tsx
+++ apps/forge/src/components/routing/Forbidden.tsx
@@ -1,31 +1,29 @@
 import Title from "@/components/title";
-import { useNavigate } from "@tanstack/react-router";
+import { RootComponentInner } from "@/routes/__root";
+import { Link } from "@tanstack/react-router";
 import { Button } from "@ui/components/ui/button.tsx";
-import React from "react";
 
 export const Forbidden = () => {
-  const navigate = useNavigate();
-
-  const goToHome = () => navigate({ to: "/" });
-
   return (
-    <React.Fragment>
-      <Title prompt="Forbidden" />
-      <div className="flex items-center justify-center w-full min-h-[80vh] px-4">
-        <div className="grid items-center gap-4 text-center">
-          <div className="space-y-2">
-            <h1 className="text-4xl font-bold tracking-tighter sm:text-6xl">403 - Forbidden</h1>
-            <p className="max-w-[600px] mx-auto text-accent-foreground md:text-xl/relaxed">
-              Some wires have been crossed, you cannot access the requested page.
-            </p>
-          </div>
-          <div className="flex justify-center">
-            <Button variant="outline" onClick={goToHome}>
-              Go to Homepage
-            </Button>
+    <RootComponentInner>
+      <>
+        <Title prompt="Forbidden" />
+        <div className="flex items-center justify-center w-full min-h-[80vh] px-4">
+          <div className="grid items-center gap-4 text-center">
+            <div className="space-y-2">
+              <h1 className="text-4xl font-bold tracking-tighter sm:text-6xl">403 - Forbidden</h1>
+              <p className="max-w-[600px] mx-auto text-accent-foreground md:text-xl/relaxed">
+                Some wires have been crossed, you cannot access the requested page.
+              </p>
+            </div>
+            <div className="flex justify-center space-x-2">
+              <Link to="/">
+                <Button variant="outline">Go to Homepage</Button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </React.Fragment>
+      </>
+    </RootComponentInner>
   );
 };
--- apps/forge/src/components/routing/Loading.tsx
+++ apps/forge/src/components/routing/Loading.tsx
@@ -1,11 +1,10 @@
 import Title from "@/components/title";
-import React from "react";
 import { Loader } from "@ui/components/ui/loader.tsx";
 
 export const Loading = () => {
   return (
-    <React.Fragment>
-      <Title prompt="Loading...!!" />
+    <>
+      <Title prompt="Loading..." />
       <div className="flex items-center justify-center w-full min-h-svh px-4">
         <div className="grid items-center gap-4 text-center">
           <div className="space-y-2">
@@ -13,6 +12,6 @@ export const Loading = () => {
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </>
   );
 };
