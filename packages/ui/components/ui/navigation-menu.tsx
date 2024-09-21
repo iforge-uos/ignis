dfd9d2018d633d23f125357ce363cc89197dbc54
Gobot1234 <gobot1234yt@gmail.com>
2024-09-21 20:00:37 +0100

    fix: styling bits and bobs from futura transition

--- packages/ui/components/ui/navigation-menu.tsx
+++ packages/ui/components/ui/navigation-menu.tsx
@@ -72,7 +72,7 @@ const NavigationMenuTrigger = React.forwardRef<
     >
       {children}
       <ChevronDown
-        className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
+        className="relative top-[1px] stroke-[4px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
         aria-hidden="true"
       />
     </NavigationMenuPrimitive.Trigger>
