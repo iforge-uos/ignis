fc6dfd2e504eb35e584fe0498d6529ef37edeae2
Sam Piper <sampii@protonmail.com>
2024-11-16 22:20:50 +0000

    fix: make user nav navigate to correct user page + update deps

--- apps/forge/src/components/app-navigation/nav-user.tsx
+++ apps/forge/src/components/app-navigation/nav-user.tsx
@@ -92,7 +92,7 @@ export function NavUser() {
               <DropdownMenuSeparator />
               <DropdownMenuGroup>
                 <DropdownMenuItem asChild>
-                  <Link to="/user">
+                  <Link to="/user/me">
                     <BadgeCheck className="mr-2 h-4 w-4" />
                     Profile
                     <DropdownMenuShortcut>{`⇧${metaKey}P`}</DropdownMenuShortcut>
