3c918ef58b5d9b26689815d66b16e91d1db536f1
Gobot1234 <gobot1234yt@gmail.com>
2025-02-24 00:59:15 +0000

    fix: revert the previous commit cause that was very bad

--- apps/anvil/dbschema/queries/addInPersonTraining.query.ts
+++ apps/anvil/dbschema/queries/addInPersonTraining.query.ts
@@ -20,8 +20,10 @@ with
     user := <users::User><uuid>$id,
 update user
 set {
-    training := (
+    training += (
         select .training {
+            @created_at := @created_at,
+            @infraction := @infraction,
             @in_person_created_at := <datetime>$created_at,
             @in_person_signed_off_by := rep.id,
         } filter .id =  <uuid>$training_id
