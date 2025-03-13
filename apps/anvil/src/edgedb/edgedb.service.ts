659794f8b6481919a173749c46aa823746a9f69b
Gobot1234 <gobot1234yt@gmail.com>
2025-03-13 00:40:27 +0000

    temp: disable access policies

--- apps/anvil/src/edgedb/edgedb.service.ts
+++ apps/anvil/src/edgedb/edgedb.service.ts
@@ -3,15 +3,13 @@ import * as T from "@dbschema/edgeql-js/typesystem";
 import { OnModuleInit } from "@nestjs/common";
 
 export class EdgeDBService implements OnModuleInit {
-  client = createClient();
+  client = createClient().withConfig({ apply_access_policies: false });
 
   async onModuleInit() {
     await this.client.ensureConnected();
   }
 
-  public async query<Expr extends T.Expression>(
-    expression: Expr,
-  ): Promise<$infer<Expr>> {
+  public async query<Expr extends T.Expression>(expression: Expr): Promise<$infer<Expr>> {
     return await expression.run(this.client);
   }
 }
