ed61178e3419d99dc321ad4c00d01ca841705bf7
Gobot1234 <gobot1234yt@gmail.com>
2025-04-29 00:48:55 +0100

    fix: i wish i cared enough to fix this

--- apps/anvil/src/root/root.controller.ts
+++ apps/anvil/src/root/root.controller.ts
@@ -171,8 +171,7 @@ export class RootController {
   }
 
   @Get("give-agreements")
-  async giveAgreements(@GetUser() user: User) {
-    if (user.id === "61363b82-f86c-11ee-8cfe-c7c36799e2a2") {await this.rootService.giveAgreements()}
-    
+  async giveAgreements() {
+    await this.rootService.giveAgreements();
   }
 }
