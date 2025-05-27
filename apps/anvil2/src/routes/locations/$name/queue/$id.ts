1a8e0e6978d048b8a21310172151f498b843b188
Gobot1234 <gobot1234yt@gmail.com>
2025-05-27 05:14:20 +0100

    feat: I considered it (storing queueplace perm)

--- apps/anvil2/src/routes/locations/$name/queue/$id.ts
+++ apps/anvil2/src/routes/locations/$name/queue/$id.ts
@@ -4,14 +4,16 @@ import { LocationNameSchema } from "@db/zod/modules/sign_in";
 import { Duration } from "gel";
 import { z } from "zod/v4";
 
-// @IdempotencyCache(60)
 export const remove = pub
-  .route({ method: "DELETE", path: "/{id}" })
+  .route({ method: "PATCH", path: "/{id}", description: "Remove a user from the queue by the places ID" })
   .input(z.object({ name: LocationNameSchema, id: z.uuid() }))
   .handler(async ({ input: { id }, context: { db } }) =>
     e
-      .delete(e.sign_in.QueuePlace, () => ({
+      .update(e.sign_in.QueuePlace, () => ({
         filter_single: { id },
+        set: {
+          notified_at: e.op(e.datetime_of_statement(), "-", Duration.from({ minutes: 20 })),
+        },
       }))
       .run(db),
   );
