7ec33c46d71ab5898ab63f07b0e0b11f0cc31b68
Gobot1234 <gobot1234yt@gmail.com>
2025-07-12 04:06:16 +0100

    fix: typo

--- apps/forge/src/routes/api.rpc.$.ts
+++ apps/forge/src/routes/api.rpc.$.ts
@@ -1,14 +1,14 @@
 import { onError } from "@orpc/server";
 import { RPCHandler } from "@orpc/server/fetch";
 import { createServerFileRoute } from "@tanstack/react-start/server";
-import { router } from "./api.$";
+import { createContext, router } from "./api.$";
 
 const handler = new RPCHandler(router, { interceptors: [onError(console.error)] });
 
 async function handle({ request }: { request: Request }) {
   const { response } = await handler.handle(request, {
     prefix: "/api/rpc",
-    context: await createContext(request), // Provide initial context if needed
+    context: await createContext({ request }), // Provide initial context if needed
   });
 
   return response ?? new Response("Not Found", { status: 404 });
@@ -21,5 +21,5 @@ export const ServerRoute = createServerFileRoute("/api/rpc/$").methods({
   PUT: handle,
   PATCH: handle,
   DELETE: handle,
-  OPTIONS: handle
+  OPTIONS: handle,
 });
