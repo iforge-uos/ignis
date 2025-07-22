b884223df8fc7ef7e57adddd7564d272343eaf72
Gobot1234 <gobot1234yt@gmail.com>
2025-07-22 22:39:38 +0100

    chore: cleanup the footer

--- apps/forge/src/components/footer/index.tsx
+++ apps/forge/src/components/footer/index.tsx
@@ -1,22 +1,15 @@
-import { orpc } from "@/lib/orpc";
-import { removeSuffix, toTitleCase } from "@/lib/utils";
+import { locationStatusesAtom } from "@/atoms/signInAppAtoms";
+import { IForgeLogo } from "@/icons/IForge";
+import { LocationIcon } from "@/icons/Locations";
+import { DiscordIcon, GitHubIcon, InstagramIcon, LinkedInIcon, TwitterIcon, YouTubeIcon } from "@/icons/Socials";
+import { exhaustiveGuard, removeSuffix, toTitleCase } from "@/lib/utils";
 import { Entries } from "@packages/types";
 import { PartialLocation } from "@packages/types/sign_in";
 import { Separator } from "@packages/ui/components/separator";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@packages/ui/components/tooltip";
-import { LocationIcon } from "@packages/ui/icons/Locations";
-import {
-  DiscordIcon,
-  GitHubIcon,
-  InstagramIcon,
-  LinkedInIcon,
-  TwitterIcon,
-  YouTubeIcon,
-} from "@packages/ui/icons/Socials";
-import { IForgeLogo } from "@packages/ui/icons/iforge";
-import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
-import {Balancer} from "react-wrap-balancer";
+import { useAtomValue } from "jotai";
+import { Balancer } from "react-wrap-balancer";
 
 function LocationStatusTooltip({ location }: { location: PartialLocation }) {
   let className: string;
@@ -36,7 +29,7 @@ function LocationStatusTooltip({ location }: { location: PartialLocation }) {
       tooltip = "Opening/Closing Soon";
       break;
     default:
-      throw new Error("Unreachable"); // TODO make this grey if the backend is down
+      exhaustiveGuard(location.status); // TODO make this grey if the backend is down
   }
   return (
     <Tooltip>
@@ -53,14 +46,10 @@ function LocationStatusTooltip({ location }: { location: PartialLocation }) {
 }
 
 export function Footer() {
-  const { data: locationStatuses } = useQuery(
-    orpc.locations.statuses.queryOptions({
-      staleTime: 20_000,
-    }),
-  );
+  const { data: locationStatuses } = useAtomValue(locationStatusesAtom);
 
   return (
-    <footer className="bg-card border-t-2 p-5 md:p-10">
+    <footer className="bg-background border-t-2 p-5 md:p-10">
       <div className="flex flex-col md:flex-row justify-between items-start md:items-end mx-2 md:mx-5 gap-8 md:gap-4">
         <div className="flex flex-col gap-2 not-prose max-w-full md:max-w-[30rem]">
           <Link to="/">
@@ -76,7 +65,7 @@ export function Footer() {
           </p>
         </div>
         <div className="flex flex-col gap-2 w-full md:w-auto">
-          <h5 className="text-lg font-bold font-futura">Opening Hours</h5>
+          <h5 className="text-2xl font-bold font-futura">Opening Hours</h5>
           <Balancer ratio="0.5">Open weekdays, subject to exams and holidays</Balancer>
           {locationStatuses && (
             <table className="w-full border-collapse items-center flex">
@@ -89,7 +78,8 @@ export function Footer() {
                     </td>
                     <td className="flex items-center justify-end">
                       <span className="text-sm md:text-base">
-                        {removeSuffix(location.opening_time, ":00")} to {removeSuffix(location.closing_time, ":00")}
+                        {removeSuffix(location.opening_time.toString(), ":00")} to{" "}
+                        {removeSuffix(location.closing_time.toString(), ":00")}
                       </span>
                       <LocationStatusTooltip location={location} />
                     </td>
@@ -100,16 +90,16 @@ export function Footer() {
           )}
         </div>
         <div className="flex flex-col gap-2">
-          <h5 className="text-lg font-bold font-futura">Website</h5>
+          <h5 className="text-2xl font-bold font-futura">Website</h5>
           <Link to="/locations" className="hover:underline underline-offset-4">
             Locations
           </Link>
-          <Link to="/projects" className="hover:underline underline-offset-4">
+          <a href="https://docs.iforge.sheffield.ac.uk/blog" className="hover:underline underline-offset-4">
             Projects
-          </Link>
-          <Link to="/about" className="hover:underline underline-offset-4">
+          </a>
+          <a href="https://docs.iforge.sheffield.ac.uk/users/about_us" className="hover:underline underline-offset-4">
             About Us
-          </Link>
+          </a>
           <Link to="/contact" className="hover:underline underline-offset-4">
             Contact
           </Link>
