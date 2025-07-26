c2702256f3834bf00ebfce09dc43456b068e9c52
Gobot1234 <gobot1234yt@gmail.com>
2025-07-26 02:13:37 +0100

    chore: reduce bundle size a lot
    numbers though are for nerds

--- apps/forge/src/components/loading/index.tsx
+++ apps/forge/src/components/loading/index.tsx
@@ -1,4 +1,4 @@
-import Lottie from "react-lottie-player";
+import Lottie from "react-lottie-player/dist/LottiePlayerLight";
 import hammerUrl from "/loaders/hammer.json?url";
 
 export function Hammer({ className }: { className?: string }) {
