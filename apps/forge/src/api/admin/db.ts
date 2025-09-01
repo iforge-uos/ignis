fee58b7b68d494f235dbbc50a37f0a18ae901614
Gobot1234 <gobot1234yt@gmail.com>
2025-09-01 01:32:53 +0100

    feat: bun for gel token generation

--- apps/forge/src/api/admin/db.ts
+++ apps/forge/src/api/admin/db.ts
@@ -1,25 +1,10 @@
-import { exec } from "node:child_process";
-import { admin } from "@/orpc";
 import { logger } from "@sentry/tanstackstart-react";
+import { $ } from "bun";
+import { admin } from "@/orpc";
 
 export const getGelUI = admin.route({ path: "/db/ui" }).handler(async () => {
   logger.info("Retrieving Gel UI URL");
 
-  const url = await new Promise<string>((resolve, reject) => {
-    exec("gel ui --print-url", (error, stdout, stderr) => {
-      if (error) {
-        logger.error(`Error executing gel ui command: ${error.message}`);
-        reject(error);
-        return;
-      }
-
-      if (stderr) {
-        logger.warn(`Command stderr: ${stderr}`);
-      }
-
-      return resolve(stdout.trim());
-    });
-  });
-
+  const url = await $`gel ui --print-url -I ignis`.text();
   return url.match(/authToken=(.*)/)?.[1]!;
 });
