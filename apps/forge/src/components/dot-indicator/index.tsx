dddd3a9ac13ac8fb9e958658ba9389d5a4c118c7
Gobot1234 <gobot1234yt@gmail.com>
2025-11-25 01:46:22 +0000

    feat: close IFO-245

--- apps/forge/src/components/dot-indicator/index.tsx
+++ apps/forge/src/components/dot-indicator/index.tsx
@@ -22,7 +22,11 @@ const DotIndicator: React.FC<DotIndicatorProps> = ({ count, current = 1, orienta
         <div
           // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
           key={index}
-          className={`w-4 h-4 rounded-lg transition-colors duration-300 border-2 ${current === index + 1 ? "border-ring" : "border-border"}`}
+          className={cn(`size-4 rounded-lg duration-500 border-2 transition-all`, current === index + 1 ? `border-ring` : "border-border")}
+          style={{
+            width: current === index + 1 && orientation === "horizontal" ? "32px" : "16px",
+            height: current === index + 1 && orientation === "vertical" ? "32px" : "16px",
+          }}
         />
       ))}
     </div>
