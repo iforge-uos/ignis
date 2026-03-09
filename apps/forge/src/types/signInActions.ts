56348b67f4987f8e8f1770e623043ae4bdf1bdb1
Gobot1234 <gobot1234yt@gmail.com>
2026-03-09 14:02:30 +0000

    chore: remove unused typing declarations for signin

--- apps/forge/src/types/signInActions.ts
+++ apps/forge/src/types/signInActions.ts
@@ -1,24 +1,30 @@
 import React from "react";
-import { StepType } from "@/api/locations/$name/sign-in/_flows/_steps";
 import z from "zod";
-import type { InitialiseToTransmitMap } from "@/routes/test/$name.$ucard_number";
+import type {
+  StepToFinaliseMap,
+  StepToReceiveMap,
+  StepToTransmitMap,
+  StepType,
+} from "@/routes/test/$name.$ucard_number";
+import { SignInSteps } from "../providers/SignInSteps";
+import type { SignInUser } from "../lib/utils/queries";
 
-export enum SignInSteps {
-  Step1 = "UCard Input",
-  Step2 = "Fetching Training",
-  Step3 = "Reason Input",
-  Step4 = "Sign In",
-}
+// export enum SignInSteps {
+//   Step1 = "UCard Input",
+//   Step2 = "Fetching Training",
+//   Step3 = "Reason Input",
+//   Step4 = "Sign In",
+// }
 
-export enum SignOutSteps {
-  Step1 = "UCard Input",
-  Step2 = "Sign Out",
-}
+// export enum SignOutSteps {
+//   Step1 = "UCard Input",
+//   Step2 = "Sign Out",
+// }
 
-export enum EnqueueSteps {
-  Step1 = "UCard Input",
-  Step2 = "Enqueue",
-}
+// export enum EnqueueSteps {
+//   Step1 = "UCard Input",
+//   Step2 = "Enqueue",
+// }
 
 export enum FlowType {
   SignIn = "SIGN_IN",
@@ -26,29 +32,29 @@ export enum FlowType {
   Enqueue = "ENQUEUE",
 }
 
-export interface StepComponentProps {
-  onPrimary: () => void;
-  onSecondary: () => void;
+export interface StepComponentProps<StepT extends StepType> {
+  data: SignInSteps<StepT>["transmit"]; // the transmit from initialise()
+  user: SignInUser,
 }
 
-export interface FlowStepComponent<StepT extends z.infer<typeof StepType>> extends React.FC<StepComponentProps & {data: InitialiseToTransmitMap[StepT]}> {}
+export interface FlowStepComponent<StepT extends StepType> extends React.FC<StepComponentProps<StepT>> {}
 
-export interface FlowConfiguration {
-  [FlowType.SignIn]: Record<SignInSteps, FlowStepComponent>;
-  [FlowType.SignOut]: Record<SignOutSteps, FlowStepComponent>;
-  [FlowType.Enqueue]: Record<EnqueueSteps, FlowStepComponent>;
-}
+// export interface FlowConfiguration {
+//   [FlowType.SignIn]: Record<SignInSteps, FlowStepComponent>;
+//   [FlowType.SignOut]: Record<SignOutSteps, FlowStepComponent>;
+//   [FlowType.Enqueue]: Record<EnqueueSteps, FlowStepComponent>;
+// }
 
 // Define a type that can be either SignInSteps or SignOutSteps.
-export type AnyStep = SignInSteps | SignOutSteps | EnqueueSteps;
+// export type AnyStep = SignInSteps | SignOutSteps | EnqueueSteps;
 
-export const flowTypeToPrintTable = (flowType: FlowType) => {
-  switch (flowType) {
-    case FlowType.SignIn:
-      return "Sign In";
-    case FlowType.SignOut:
-      return "Sign Out";
-    case FlowType.Enqueue:
-      return "Enqueue";
-  }
-};
+// export const flowTypeToPrintTable = (flowType: FlowType) => {
+//   switch (flowType) {
+//     case FlowType.SignIn:
+//       return "Sign In";
+//     case FlowType.SignOut:
+//       return "Sign Out";
+//     case FlowType.Enqueue:
+//       return "Enqueue";
+//   }
+// };
