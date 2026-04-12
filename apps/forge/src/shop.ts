9d58fd7de6cad13aeaaf433014ce1a797276dac8
Gobot1234 <gobot1234yt@gmail.com>
2026-04-13 00:43:13 +0100

    feat: update pharos url

--- apps/forge/src/shop.ts
+++ apps/forge/src/shop.ts
@@ -7,8 +7,8 @@ try {
   const timeoutPromise = new Promise((_, reject) =>
     setTimeout(() => reject(new Error('Pharos client creation timed out after 3s')), 3_000)
   );
-  const clientPromise = PharosClient.create(`http://localhost:3002/pharosedi/ediservice.asmx?wsdl`, env.pharos.auth);
-  // const clientPromise = PharosClient.create(`${env.pharos.url}?wsdl`, env.pharos.auth);
+  // const clientPromise = PharosClient.create(`http://localhost:3002/pharosedi/ediservice.asmx?wsdl`, env.pharos.auth);
+  const clientPromise = PharosClient.create(`${env.pharos.url}?wsdl`, env.pharos.auth);
 
   client = await Promise.race([clientPromise, timeoutPromise]) as PharosClient;
 } catch (error) {
