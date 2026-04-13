82902b638396d5b059619be5bbe74225a71bc824
Gobot1234 <gobot1234yt@gmail.com>
2026-04-13 13:45:43 +0100

    fix: remove pointless polyfil

--- apps/forge/src/polyfill.ts
+++ apps/forge/src/polyfill.ts
@@ -1,17 +1,6 @@
 import { Temporal, toTemporalInstant } from "@js-temporal/polyfill";
 import { implementation } from "regexp.escape";
 
-const OriginalRequest = globalThis.Request;
-
-globalThis.Request = class Request extends OriginalRequest {
-  constructor(input: RequestInfo | URL, init?: RequestInit) {
-    super(input, {
-      ...init,
-      duplex: "half",
-    });
-  }
-};
-
 declare global {
   interface Date {
     toTemporalInstant(): Temporal.Instant;
