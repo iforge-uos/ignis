598a0e4fd450c7e7d343d7fb22e9123fe13de6ce
Gobot1234 <gobot1234yt@gmail.com>
2026-04-22 14:05:50 +0100

    fix: missed link from redirect

--- apps/forge/src/routes/_authenticated/user/agreements/-components/AgreementCard.tsx
+++ apps/forge/src/routes/_authenticated/user/agreements/-components/AgreementCard.tsx
@@ -20,7 +20,7 @@ export function AgreementCard({ agreement }: AgreementCardProps) {
   const user = useUser()!;
 
   const handleViewAgreement = () => {
-    navigate({ to: "/sign-in/agreements/$id", params: agreement });
+    navigate({ to: "/user/agreements/$id", params: agreement });
   };
 
   const getAgreementStatus = (agreement: Agreement) => {
